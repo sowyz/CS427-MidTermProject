6b40bb627610d4e15e6e081814d0c965c50cab93
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2024-06-21 22:18:02 +0700

    Add UI assets and write Start() Quit()

---
+++ Project/Assets/Script/StartMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
