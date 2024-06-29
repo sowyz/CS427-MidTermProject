bd3305592739dafe6f33d1318fd78490dca52c97
Duc7111 <phantrungduc2003@gmail.com>
2024-06-29 17:33:29 +0700

    AI update and player-following camera

--- Project/Assets/Script/AI/ShootBehavior.cs
+++ Project/Assets/Script/AI/ShootBehavior.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class ShootBehavior : AIBehavior
 {
     public float fieldOfVision = 60f;
+    public float spacing = 10f;
 
     public override void performAction(TankController tank, Transform target)
     {
         if(TargetInFOV(tank, target))
         {
-            tank.HandleMoveBody(Vector2.zero);
             tank.HandleShoot();
         }
-        else
+        if (target != null && Vector2.Distance(tank.transform.position, target.position) > spacing)
         {
-            tank.HandleMoveBody((target.position - tank.transform.position).normalized);
+            Vector2 direction = (Vector2)target.position - (Vector2)tank.tankMover.transform.position;
+            var dotProduct = Vector2.Dot(direction.normalized, tank.tankMover.transform.up);
+            if (dotProduct < 0.98f)
+            {
+                var crossProduct = Vector3.Cross(tank.tankMover.transform.up, direction.normalized);
+                int rotationDirection = crossProduct.z > 0 ? -1 : 1;
+                tank.HandleMoveBody(new Vector2(rotationDirection, 1));
+            }
+            else
+            {
+                tank.HandleMoveBody(Vector2.up);
+            }
         }
         tank.HandleTurretMovement(target.position);
     }
@@ -26,4 +38,11 @@ public class ShootBehavior : AIBehavior
         float angle = Vector2.Angle(tank.aimTurret.transform.right, target.position - tank.aimTurret.transform.position);
         return angle < fieldOfVision;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, spacing);
+    }
 }   
+
